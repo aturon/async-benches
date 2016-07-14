6273d831c3ecd4cf70578bb9b641944d190dfd25
Alex Crichton <alex@alexcrichton.com>
2016-07-13 22:40:51 -0700

    Add hyper to techempower-6

---
+++ techempower-6/hyper-master/src/main.rs
@@ -0,0 +1,65 @@
+#[macro_use]
+extern crate log;
+extern crate hyper;
+extern crate env_logger;
+extern crate mime;
+
+use std::io::Write;
+use std::env;
+use std::net::SocketAddr;
+
+use mime::Mime;
+use mime::TopLevel::Text;
+use mime::SubLevel::Plain;
+
+use hyper::{RequestUri, Decoder, Encoder, Next};
+use hyper::header::{ContentLength, ContentType, Server as ServerHeader};
+use hyper::net::HttpStream;
+use hyper::server::{Server, Handler, Request, Response};
+
+struct Plaintext;
+
+const INDEX: &'static [u8] = b"Hello, World!";
+
+impl Handler<HttpStream> for Plaintext {
+    fn on_request(&mut self, req: Request<HttpStream>) -> Next {
+        match *req.uri() {
+            RequestUri::AbsolutePath(ref path) => {
+                assert_eq!(&path[..], "/plaintext");
+                Next::write()
+            }
+            _ => panic!("wrong uri?"),
+        }
+    }
+
+    fn on_request_readable(&mut self,
+                           _transport: &mut Decoder<HttpStream>) -> Next {
+        panic!()
+    }
+
+    fn on_response(&mut self, res: &mut Response) -> Next {
+        let data = (mime::Attr::Charset, mime::Value::Utf8);
+        res.headers_mut().set(ContentType(Mime(Text, Plain, vec![data])));
+        res.headers_mut().set(ContentLength(INDEX.len() as u64));
+        res.headers_mut().set(ServerHeader("Example".to_string()));
+        Next::write()
+    }
+
+    fn on_response_writable(&mut self, transport: &mut Encoder<HttpStream>) -> Next {
+        transport.write(INDEX).unwrap();
+        Next::end()
+    }
+}
+
+// TODO: this program appears to not work with pipelined wrk requests?
+
+fn main() {
+    env_logger::init().unwrap();
+
+    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let addr = addr.parse::<SocketAddr>().unwrap();
+    let server = Server::http(&addr).unwrap();
+    let (listening, server) = server.handle(|_| Plaintext).unwrap();
+    println!("Listening on http://{}", listening);
+    server.run();
+}
