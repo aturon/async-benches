d1671c3ff1cc35112be6e78dc767ab54781b9a88
Alex Crichton <alex@alexcrichton.com>
2016-08-06 18:13:31 -0700

    Print out connection rates in the server

--- tcp-throughput/server/src/main.rs
+++ tcp-throughput/server/src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
 use std::io::*;
 use std::net::*;
+use std::sync::atomic::*;
+use std::thread;
+use std::time::Duration;
+
+static AMT: AtomicUsize = ATOMIC_USIZE_INIT;
 
 fn main() {
     let addr = env::args().nth(1).unwrap_or("127.0.0.1:12345".to_string());
@@ -8,10 +13,18 @@ fn main() {
     let l = TcpListener::bind(&addr).unwrap();
     println!("listening...");
     let b = [0; 64 * 1024];
+    thread::spawn(|| {
+        loop {
+            thread::sleep(Duration::new(1, 0));
+            println!("{}", AMT.swap(0, Ordering::SeqCst));
+        }
+    });
     loop {
         let mut c = l.accept().unwrap().0;
         println!("got a connection, writing some data to it");
-        while c.write(&b).is_ok() {}
+        while let Ok(n) = c.write(&b) {
+            AMT.fetch_add(n, Ordering::SeqCst);
+        }
         println!("connection is gone, listening for another");
     }
 }
